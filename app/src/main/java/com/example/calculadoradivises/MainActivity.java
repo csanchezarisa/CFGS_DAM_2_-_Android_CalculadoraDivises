83b28b7d8ef3e92d3a564d7678f9f748309170b1
csanchezarisa <cristobal.sanchez@mataro.epiaedu.cat>
2020-10-08 20:01:23 +0200

    Botones asignados en código

--- app/src/main/java/com/example/calculadoradivises/MainActivity.java
+++ app/src/main/java/com/example/calculadoradivises/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.calculadoradivises;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView txtInput = (TextView) findViewById(R.id.txtInput);
+        TextView txtOutput = (TextView) findViewById(R.id.txtOutput);
+        Button btn0 = (Button) findViewById(R.id.btn0);
+        Button btn1 = (Button) findViewById(R.id.btn1);
+        Button btn2 = (Button) findViewById(R.id.btn2);
+        Button btn3 = (Button) findViewById(R.id.btn3);
+        Button btn4 = (Button) findViewById(R.id.btn4);
+        Button btn5 = (Button) findViewById(R.id.btn5);
+        Button btn6 = (Button) findViewById(R.id.btn6);
+        Button btn7 = (Button) findViewById(R.id.btn7);
+        Button btn8 = (Button) findViewById(R.id.btn8);
+        Button btn9 = (Button) findViewById(R.id.btn9);
+        Button btnComa = (Button) findViewById(R.id.btnComa);
+        Button btnCE= (Button) findViewById(R.id.btnCE);
+        Button btnBorrar = (Button) findViewById(R.id.btnBorrar);
+        Button btnIgual = (Button) findViewById(R.id.btnIgual);
+
+
+
     }
 }
